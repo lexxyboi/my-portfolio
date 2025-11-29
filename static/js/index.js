0f0b70be913faae79acbc7e1d1011124d947b0f2
Lex Ortiz <kingtiva@gmail.com>
2025-11-29 23:56:35 +0800

    Hard reset upload

---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+// CONTACT ME BUTTON LOGIC
+const emailButton = document.getElementById("contact-btn-hero");
+emailButton.addEventListener("click", function () {
+  window.location.href = "mailto:lex.ortiz@g.msuiit.edu.ph";
+});
